655bd93fa444e69a2f9d6a4d9df2edb78b06370d
insta_clone_2 <vaishnavikadtan04@gmail.com>
2026-08-19 15:40:53 +0530

    added function folder

---
+++ 08_function/src/App.jsx
@@ -0,0 +1,54 @@
+// import React from "react";
+
+// const App = () => {
+  // function inputChanging(val) {
+  //   console.log(val);
+  // } 
+
+  // const pageScrolling = (elem)=>{
+  //   if(elem>0){
+  //     console.log('seedha scrolling');
+      
+  //   }else{
+  //     console.log('ulta scrolling');
+      
+  //   }
+
+  // }
+  // return (
+    // <div>
+     
+     {/* <div onWheel={(elem)=>{
+      pageScrolling(elem.deltaY)
+     }}>
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div> */}
+
+    //  </div>
+
+
+
+
+
+
+      {/* <input
+        onChange={function (elem) {
+          inputChanging(elem.target.value);
+        }}
+        type="text"
+        placeholder="Enter Name" */}
+      {/* /> */}
+
+      {/* <h1>Hello, Vaishnavi</h1>
+      <button onClick={() => { console.log("Button Clicked");}}>
+        Change user
+      </button> */}
+    // </div>
+  // );
+// };
+
+// export default App;
+
+
+ 
